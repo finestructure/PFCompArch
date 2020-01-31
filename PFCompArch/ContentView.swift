627f3ddc53a7483237e1fc46625f9ed8203feed6
Sven A. Schmidt <sas@finestructure.co>
2020-01-31 07:49:56 +0100

    Add store

--- PFCompArch/ContentView.swift
+++ PFCompArch/ContentView.swift
@@ -6,16 +6,33 @@
 //  Copyright © 2020 finestructure. All rights reserved.
 //
 
+import CompArch
 import SwiftUI
 
+struct AppState {
+    var items: [String]
+}
+
+enum AppAction {
+    case itemTapped
+}
+
 struct ContentView: View {
+    @ObservedObject var store: Store<AppState, AppAction>
+
     var body: some View {
-        Text("Hello, World!")
+        List(store.value.items, id: \.self) {
+            Text($0)
+        }
     }
+}
+
+let appReducer: Reducer<AppState, AppAction> = { (value, action) in
+    return []
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: .init(initialValue: .init(items: ["a", "b", "c"]), reducer: appReducer))
     }
 }
