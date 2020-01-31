ae8f52031071d303c99baba35490a0e4e21f9be2
Sven A. Schmidt <sas@finestructure.co>
2020-01-31 07:28:58 +0100

    Initial Commit

---
+++ PFCompArch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PFCompArch
+//
+//  Created by Sven A. Schmidt on 31/01/2020.
+//  Copyright © 2020 finestructure. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
